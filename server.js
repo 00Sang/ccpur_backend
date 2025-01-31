ba967a2fc25b5e4410f2cff1a18f6c9f1e862442
00Sang <p.biaksang143@gmail.com>
2025-02-01 01:03:37 +0530

    new file:   controller/newApplicationForm.js 	new file:   controller/userController.js 	new file:   models/newApplication.js 	new file:   models/userModel.js 	modified:   package-lock.json 	modified:   package.json 	new file:   router/route.js 	modified:   server.js

--- server.js
+++ server.js
@@ -1,36 +1,32 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require("dotenv");
-const userRouter = require('./route/route.js');
-require('dotenv').config();
+const router = require('./router/route');
 
+const app = express();
 
 // Initialize dotenv to access environment variable
 const port = process.env.PORT;
 dotenv.config();
 
 //Middleware
-const app = express();
 app.use(cors());
 app.use(express.json());// req.body
-app.use("/api/user", userRouter);
+app.use(express.urlencoded({ extended: true }));
 
-// Unknown route handler
-app.use((err, req, res) => {
-  // Log the error and the URL where it occurred
-  console.error('Error:', err.message);
-  //log the URL that triggered the error
-  console.log('Request URL that caused the error:', req.url);
-   // Log the query parameters from the request (if any)
-   console.log('Query parameters:', req.query);
-   // Log the headers sent with the request
-   console.log('Request headers:', req.headers);
+//Route Middleware
+app.use("/api/user", router);
 
-  // Respond with a generic error message and status 500
-  res.status(500).send('Route not found');
+// Handle unknown routes
+app.all("*", (req, res) => {
+  res.status(404).json({ error: "Route not found" });
 });
 
-
+// General Error Handler
+app.use((err, req, res, next) => {
+  console.error("Error:", err);
+  res.status(500).json({ error: "Internal Server Error" });
+});
 
 app.listen(port, () =>{
   console.log(`Server is running on port ${port}`);
